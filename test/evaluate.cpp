a1c362778f9e9fc2a7060d301cd538dc01c91c68
Florian Mahlknecht <m@florian.world>
2021-03-29 17:32:43 +0200

    Implements folder output from evaluation script:   - removes synchronization of GT and estimates (do this in Python)   - output all the available timing info to realtime.csv   - copy configuration yaml to output for reproducibility

--- test/evaluate.cpp
+++ test/evaluate.cpp
@@ -6,6 +6,7 @@
 #include <rosbag/bag.h>
 #include <rosbag/view.h>
 #include <iostream>
+#include <filesystem>
 #include <yaml-cpp/yaml.h>
 #include <easy/profiler.h>
 
@@ -19,6 +20,16 @@
 #include <sensor_msgs/Imu.h>
 #include <x_vio_ros/ros_utils.h>
 #include <boost/progress.hpp>
+
+namespace fs = std::filesystem;
+
+DEFINE_string(input_bag, "", "filename of the bag to scan");
+//DEFINE_string(events_topic, "/cam0/events", "topic in rosbag publishing dvs_msgs::EventArray");
+DEFINE_string(image_topic, "/cam0/image_raw", "topic in rosbag publishing sensor_msgs::Image");
+DEFINE_string(pose_topic, "", "(optional) topic publishing IMU pose ground truth as geometry_msgs::PoseStamped");
+DEFINE_string(imu_topic, "/imu", "topic in rosbag publishing sensor_msgs::Imu");
+DEFINE_string(params_file, "", "filename of the params.yaml to use");
+DEFINE_string(output_folder, "", "folder where to write output files, is created if not existent");
 
 
 using PoseCsv = x::CsvWriter<std::string,
@@ -36,144 +47,171 @@ void addPose(PoseCsv& csv, const std::string& update_modality, const x::State& s
              s.getOrientation().x(), s.getOrientation().y(), s.getOrientation().z(), s.getOrientation().w());
 }
 
-
-
-DEFINE_string(input_bag, "", "filename of the bag to scan");
-//DEFINE_string(events_topic, "/cam0/events", "topic in rosbag publishing dvs_msgs::EventArray");
-DEFINE_string(image_topic, "/cam0/image_raw", "topic in rosbag publishing sensor_msgs::Image");
-DEFINE_string(pose_topic, "", "(optional) topic publishing IMU pose ground truth as geometry_msgs::PoseStamped");
-DEFINE_string(imu_topic, "/imu", "topic in rosbag publishing sensor_msgs::Imu");
-DEFINE_string(params_file, "", "filename of the params.yaml to use");
+char* get_time_string_in_utc() {
+  std::time_t curr_time;
+  curr_time = std::time(nullptr);
+  tm *tm_gmt = std::gmtime(&curr_time);
+  return std::asctime(tm_gmt);
+}
 
 
 int main(int argc, char **argv) {
-  google::ParseCommandLineFlags(&argc, &argv, true);
+  {
 
-  PoseCsv pose_csv("pose.csv", {"update_modality", "t",
-                                "estimated_p_x", "estimated_p_y", "estimated_p_z",
-                                "estimated_q_x", "estimated_q_y", "estimated_q_z", "estimated_q_w"});
+    std::cerr << "Running " << argv[0] << " " << get_time_string_in_utc() << std::endl;
 
-  GTCsv gt_csv("gt.csv", {"t_gt", "closest_gt_p_x", "closest_gt_p_y", "closest_gt_p_z",
-                          "closest_gt_q_x", "closest_gt_q_y", "closest_gt_q_z", "closest_gt_q_w"});
+    google::ParseCommandLineFlags(&argc, &argv, true);
 
-
-  x::CsvWriter<double, double, double> rt_csv("realtime.csv", {"t_sim", "t_real", "rt_factor"});
-
-  // directly reads yaml file, without the need for a ROS master / ROS parameter server
-  YAML::Node config = YAML::LoadFile(FLAGS_params_file);
-  x::ParameterLoader l;
-  x::Params params;
-  auto success = l.loadXParamsWithYamlFile(config, params);
-  std::cerr << "Reading config '" << FLAGS_params_file << "' was " << (success? "successful" : "failing") << std::endl;
-
-  if (!success)
-    return 1;
-
-  std::cerr << "Reading rosbag '" << FLAGS_input_bag << "'" << std::endl;
-  rosbag::Bag bag;
-  bag.open(FLAGS_input_bag);  // BagMode is Read by default
-
-
-  x::VIO vio;
-  vio.setUp(params);
-
-  rosbag::View view(bag);
-
-  std::cerr << "Initializing at time " << view.getBeginTime().toSec() << std::endl;
-  vio.initAtTime(view.getBeginTime().toSec());
-
-  std::cerr << "Processing rosbag from time " << view.getBeginTime() << " to " << view.getEndTime()
-            << std::endl << std::endl;
-
-  uint64_t counter_imu = 0, counter_image = 0, counter_events = 0, counter_pose = 0;
-  bool filer_initialized = false;
-  boost::progress_display show_progress(view.size(), std::cerr);
-
-  double rt_factor_resolution = .5;
-  double next_rt_factor = rt_factor_resolution;
-  profiler::timestamp_t calculation_time = 0, last_calculation_time = 0;
-
-  EASY_PROFILER_ENABLE;
-
-  for (rosbag::MessageInstance const &m : view) {
-    bool processing_useful_message = false;
-
-    if (m.getTopic() == FLAGS_imu_topic) {
-      processing_useful_message = true;
-      EASY_BLOCK("IMU Message");
-      auto msg = m.instantiate<sensor_msgs::Imu>();
-      ++counter_imu;
-
-      auto a_m = x::msgVector3ToEigen(msg->linear_acceleration);
-      auto w_m = x::msgVector3ToEigen(msg->angular_velocity);
-
-      auto state = vio.processImu(msg->header.stamp.toSec(), msg->header.seq, w_m, a_m);
-      addPose(pose_csv, "IMU", state);
-
-    } else if (m.getTopic() == FLAGS_image_topic) {
-      processing_useful_message = true;
-      EASY_BLOCK("Image Message", profiler::colors::Green);
-      auto msg = m.instantiate<sensor_msgs::Image>();
-      ++counter_image;
-
-      x::TiledImage image;
-      if (!x::msgToTiledImage(params, msg, image))
-        continue;
-      x::TiledImage feature_img(image);
-      auto state = vio.processImageMeasurement(image.getTimestamp(), image.getFrameNumber(), image, feature_img);
-      addPose(pose_csv, "Image", state);
-
-//    } else if (m.getTopic() == FLAGS_events_topic) {
-//      processing_useful_message = true;
-//      auto msg = m.instantiate<dvs_msgs::EventArray>();
-//      ++counter_events;
-
-    } else if (m.getTopic() == FLAGS_pose_topic) {
-      EASY_BLOCK("GT Message", profiler::colors::Yellow);
-      auto p = m.instantiate<geometry_msgs::PoseStamped>();
-      ++counter_pose;
-      gt_csv.addRow(p->header.stamp.toSec(), p->pose.position.x, p->pose.position.y, p->pose.position.z,
-                    p->pose.orientation.x, p->pose.orientation.y, p->pose.orientation.z, p->pose.orientation.w);
+    if (FLAGS_output_folder.empty()) {
+      std::cerr << "ERROR: No output folder specified, provide --output_folder" << std::endl;
+      return 1;
     }
 
-    if (processing_useful_message && filer_initialized) {
-      // frame time is the duration of the last ended "root" block
-      auto duration_in_us  = profiler::this_thread_frameTime();
-      calculation_time += duration_in_us;
+    fs::path output_path(FLAGS_output_folder);
 
-      double rt_factor = std::numeric_limits<double>::quiet_NaN();
+    PoseCsv pose_csv(output_path / "pose.csv", {"update_modality", "t",
+                                                "estimated_p_x", "estimated_p_y", "estimated_p_z",
+                                                "estimated_q_x", "estimated_q_y", "estimated_q_z", "estimated_q_w"});
 
-      if (m.getTime().toSec() >= next_rt_factor) {
-        next_rt_factor += rt_factor_resolution;
+    GTCsv gt_csv(output_path / "gt.csv", {"t", "closest_p_x", "closest_p_y", "closest_p_z",
+                                          "closest_q_x", "closest_q_y", "closest_q_z", "closest_q_w"});
 
-        rt_factor = static_cast<double>(calculation_time-last_calculation_time) * 1e-6 / rt_factor_resolution;
 
-        // reset calculation time
-        last_calculation_time = calculation_time;
+    x::CsvWriter<double, double, std::string, profiler::timestamp_t, double> rt_csv(output_path / "realtime.csv",
+                                                                                    {"t_sim", "t_real",
+                                                                                     "processing_type",
+                                                                                     "process_time_in_us",
+                                                                                     "rt_factor"});
+
+    // directly reads yaml file, without the need for a ROS master / ROS parameter server
+    YAML::Node config = YAML::LoadFile(FLAGS_params_file);
+    x::ParameterLoader l;
+    x::Params params;
+    auto success = l.loadXParamsWithYamlFile(config, params);
+    std::cerr << "Reading config '" << FLAGS_params_file << "' was " << (success ? "successful" : "failing")
+              << std::endl;
+
+    if (!success)
+      return 1;
+
+    fs::create_directories(output_path);
+    fs::copy(FLAGS_params_file, output_path / "params.yaml", fs::copy_options::overwrite_existing);
+
+    std::cerr << "Reading rosbag '" << FLAGS_input_bag << "'" << std::endl;
+    rosbag::Bag bag;
+    bag.open(FLAGS_input_bag);  // BagMode is Read by default
+
+
+    x::VIO vio;
+    vio.setUp(params);
+
+    rosbag::View view(bag);
+
+    std::cerr << "Initializing at time " << view.getBeginTime().toSec() << std::endl;
+    vio.initAtTime(view.getBeginTime().toSec());
+
+    std::cerr << "Processing rosbag from time " << view.getBeginTime() << " to " << view.getEndTime()
+              << std::endl << std::endl;
+
+    uint64_t counter_imu = 0, counter_image = 0, counter_events = 0, counter_pose = 0;
+    bool filer_initialized = false;
+    x::State state;
+    boost::progress_display show_progress(view.size(), std::cerr);
+
+    double rt_factor_resolution = .5;
+    double next_rt_factor = rt_factor_resolution;
+    profiler::timestamp_t calculation_time = 0, last_calculation_time = 0;
+
+    EASY_PROFILER_ENABLE;
+
+    for (rosbag::MessageInstance const &m : view) {
+      std::string process_type;
+
+      if (m.getTopic() == FLAGS_imu_topic) {
+        EASY_BLOCK("IMU Message", profiler::colors::Red);
+        process_type = "IMU";
+        auto msg = m.instantiate<sensor_msgs::Imu>();
+        ++counter_imu;
+
+        auto a_m = x::msgVector3ToEigen(msg->linear_acceleration);
+        auto w_m = x::msgVector3ToEigen(msg->angular_velocity);
+
+        state = vio.processImu(msg->header.stamp.toSec(), msg->header.seq, w_m, a_m);
+        EASY_END_BLOCK;
+
+      } else if (m.getTopic() == FLAGS_image_topic) {
+        EASY_BLOCK("Image Message", profiler::colors::Green);
+        process_type = "Image";
+        auto msg = m.instantiate<sensor_msgs::Image>();
+        ++counter_image;
+
+        x::TiledImage image;
+        if (!x::msgToTiledImage(params, msg, image))
+          continue;
+        x::TiledImage feature_img(image);
+        state = vio.processImageMeasurement(image.getTimestamp(), image.getFrameNumber(), image, feature_img);
+        EASY_END_BLOCK;
+
+        //    } else if (m.getTopic() == FLAGS_events_topic) {
+        //      processing_useful_message = true;
+        //      auto msg = m.instantiate<dvs_msgs::EventArray>();
+        //      ++counter_events;
+
+      } else if (m.getTopic() == FLAGS_pose_topic) {
+        EASY_BLOCK("GT Message");
+        auto p = m.instantiate<geometry_msgs::PoseStamped>();
+        ++counter_pose;
+        gt_csv.addRow(p->header.stamp.toSec(), p->pose.position.x, p->pose.position.y, p->pose.position.z,
+                      p->pose.orientation.x, p->pose.orientation.y, p->pose.orientation.z, p->pose.orientation.w);
+        EASY_END_BLOCK;
       }
 
-      rt_csv.addRow(m.getTime().toSec(), calculation_time*1e-6, rt_factor);
+      if (!filer_initialized && vio.isInitialized()) {
+        filer_initialized = true;
+        next_rt_factor = m.getTime().toSec() + rt_factor_resolution;
+        auto count = show_progress.count();
+        show_progress.restart(view.size());
+        show_progress += count;
+      }
+
+      if (!process_type.empty() && filer_initialized) {
+        auto duration_in_us = profiler::this_thread_frameTime();
+        calculation_time += duration_in_us;
+
+        double rt_factor = std::numeric_limits<double>::quiet_NaN();
+
+        if (m.getTime().toSec() >= next_rt_factor) {
+          next_rt_factor += rt_factor_resolution;
+
+          rt_factor = static_cast<double>(calculation_time - last_calculation_time) * 1e-6 / rt_factor_resolution;
+
+          // reset calculation time
+          last_calculation_time = calculation_time;
+        }
+
+        addPose(pose_csv, process_type, state);
+        rt_csv.addRow(m.getTime().toSec(), calculation_time * 1e-6, process_type, duration_in_us, rt_factor);
+      }
+
+      ++show_progress;
     }
 
-    if (!filer_initialized && vio.isInitialized()) {
-      filer_initialized = true;
-      next_rt_factor = m.getTime().toSec() + rt_factor_resolution;
-      auto count = show_progress.count();
-      show_progress.restart(view.size());
-      show_progress += count;
-    }
+    profiler::dumpBlocksToFile((output_path / "profiling.prof").c_str());
 
-    ++show_progress;
+    std::cerr << "Processed " << counter_imu << " IMU, "
+              << counter_image << " image, "
+              << counter_events << " events and "
+              << counter_pose << " pose messages" << std::endl;
+
+    std::cerr << "Writing outputs to folder " << output_path << std::endl;
+
+    bag.close();
+
+    // destructor calls (--> CSV flushing) happening here
   }
-
-  profiler::dumpBlocksToFile("test.prof");
-
-  std::cerr << "Processed " << counter_imu << " IMU, "
-            << counter_image << " image, "
-            << counter_events << " events and "
-            << counter_pose << " pose messages" << std::endl;
-
-  bag.close();
+  std::cerr << "Evaluation completed " << get_time_string_in_utc();
+  std::cerr << "Good bye!" << std::endl;
+  return 0;
 }
 
 
