f44ac56821214e096afa113764dbc5f6c3d1b333
Florian Mahlknecht <m@florian.world>
2021-04-27 19:23:12 +0200

    Fixes setup.py to work with "pip install ."

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ setup(
     scripts=['test/evaluate.py'],
     packages=['x_evaluate'],
     package_dir={'x_evaluate': 'src/x_evaluate'},
-    requires=['numpy>=1.19.2',
-              'matplotlib>=3.3.4',
-              'envyaml>=1.7',
-              'evo>=1.13.4',
-              'gitpython>=3.1.14',
-              'pyyaml>=5.4.1']
+    install_requires=['numpy>=1.19.2',
+                      'matplotlib>=3.3.4',
+                      'envyaml>=1.7',
+                      'evo>=1.13.4',
+                      'gitpython>=3.1.14',
+                      'pyyaml>=5.4.1']
 )
