Implements new trajectory plots and scripts to debug ESIM spline derivatives. Adds new initial state extraction script. Some further minor fixes.

scripts/search_best_tables.py
@@ -12,6 +12,7 @@ class Dataset(Enum):
     RPG_DAVIS = 'RPG_DAVIS'
     RPG_DAVIS_ROTATION = 'RPG_DAVIS_ROTATION'
     RPG_FPV = 'RPG_FPV'
+    SIM = 'SIM'
 
 
 def main():
@@ -24,6 +25,9 @@ def main():
     matches.sort()
 
     for input_file in matches:
+        # if 'sim' not in input_file:
+        #     continue
+
         print(F"######################################################################################################")
         print(F"# Scanning {input_file} #")
         print(F"######################################################################################################")
@@ -37,6 +41,8 @@ def main():
             dataset = Dataset.RPG_DAVIS_ROTATION
         elif 'fpv' in input_file:
             dataset = Dataset.RPG_FPV
+        elif 'sim' in input_file:
+            dataset = Dataset.SIM
 
         # print(tables)
 
@@ -44,13 +50,20 @@ def main():
             Dataset.RPG_DAVIS: ["Boxes 6DOF", "Boxes Translation", "Dynamic 6DOF", "Dynamic Translation", "HDR Boxes",
                                 "HDR Poster", "Poster 6DOF", "Poster Translation", "Shapes 6DOF", "Shapes Translation"],
             Dataset.RPG_DAVIS_ROTATION: ["Boxes Rotation", "Dynamic Rotation", "Poster Rotation", "Shapes Rotation"],
-            Dataset.RPG_FPV: ['Indoor 45 Seq 12', 'Indoor 45 Seq 2']
+            Dataset.RPG_FPV: ['Indoor 45 Seq 12', 'Indoor 45 Seq 2'],
+            Dataset.SIM: ["Mars Straight Vmax 3.2 Offset 2.5", "Mars Eight Vmax 3.5 Offset 2.5",
+                          "Mars Circle Vmax 7.2 Offset 2.5", "Mars Vertical Circle Vmax 2.4 Offset 2.5",
+                          "Mars Straight Vmax 3.2 Offset 5", "Mars Eight Vmax 3.5 Offset 5",
+                          "Mars Circle Vmax 7.2 Offset 5", "Mars Vertical Circle Vmax 2.4 Offset 5",
+                          "Mars Straight Vmax 3.2 Offset 10", "Mars Eight Vmax 3.5 Offset 10",
+                          "Mars Circle Vmax 7.2 Offset 10", "Mars Vertical Circle Vmax 2.4 Offset 10"],
         }
 
         sequence_lengths = {
             Dataset.RPG_DAVIS: [69.9, 65.2, 39.6, 30.1, 55.1, 55.4, 61.1, 49.3, 47.6, 56.1],
             Dataset.RPG_DAVIS_ROTATION: [14.9, 10.5, 16.9, 15.7],
             Dataset.RPG_FPV: [118.4, 176.2],
+            Dataset.SIM: [8.2, 49.5, 141.3, 30.8, 8.2, 49.5, 141.3, 30.8, 8.2, 49.5, 141.3, 30.8],
         }
 
         sequences = dataset_sequences[dataset]
@@ -60,14 +73,16 @@ def main():
             "max": lambda p: p.max(),
             "mean": lambda p: p.mean(),
             "median": lambda p: p.median(),
-            "median+max": lambda p: p.median() + 0.25*p.max(),
+            "median+max": lambda p: p.median() + 0.25 * p.max(),
         }
 
         if dataset == Dataset.RPG_DAVIS:
             def get_uslam_compare_func(uslam_results):
                 def better_than_uslam(p):
                     return len(uslam_results) - np.count_nonzero(p <= uslam_results)
+
                 return better_than_uslam
+
             stats['uslam_global'] = get_uslam_compare_func([0.68, 1.12, 0.76, 0.63, 1.01, 1.48, 0.59, 0.24, 1.07, 1.36])
             stats['uslam_best'] = get_uslam_compare_func([0.30, 0.27, 0.19, 0.18, 0.37, 0.31, 0.28, 0.12, 0.10, 0.26])
 
@@ -110,7 +125,8 @@ def main():
 
                 if best_per_sequence.loc[s, "Mean Position Error [%]"] > table.loc[s, "Mean Position Error [%]"]:
                     best_per_sequence.loc[s, "Mean Position Error [%]"] = table.loc[s, "Mean Position Error [%]"]
-                    best_per_sequence.loc[s, "Mean Rotation error [deg/m]"] = table.loc[s, "Mean Rotation error [deg/m]"]
+                    best_per_sequence.loc[s, "Mean Rotation error [deg/m]"] = table.loc[
+                        s, "Mean Rotation error [deg/m]"]
                     best_per_sequence.loc[s, "Completion rate [%]"] = table.loc[s, "Completion rate [%]"]
                     best_per_sequence.loc[s, "File"] = k
 
@@ -123,7 +139,6 @@ def main():
         #     index = np.argmin(pos_error_values[s])
         #     print(F"{s}: {pos_error_values[s][index]}  {rot_error_values[s][index]} from '{list(tables.keys())[index]}'")
 
-
         for stat, table_key in best_stats_keys.items():
             print(F"\nBest {stat} (={best_stats[stat]} table {table_key}:")
             table = tables[table_key]
@@ -135,7 +150,6 @@ def main():
         with pd.ExcelWriter(output_filename) as writer:
             best_per_sequence.to_excel(writer)
 
-
         paths_best_per_seq = [os.path.normpath(os.path.join(x, "../../")) for x in best_per_sequence['File'].values]
         paths_best_overall = [os.path.normpath(os.path.join(x, "../../")) for x in best_stats_keys.values()]
 
@@ -145,6 +159,7 @@ def main():
         print()
         print("The following directories contain some best run: ")
         print(paths)
+
 
 if __name__ == '__main__':
     main()

src/x_evaluate/plots.py
@@ -145,6 +145,38 @@ def time_series_plot(pc: PlotContext, time, data, labels, title="", ylabel=None,
         ax.set_yscale('log')
 
 
+def time_series_plot_with_ref(pc: PlotContext, time, data, data_ref, labels, title="", ylabel=None,
+                              use_log=False, xlabel=None, subplot_arg=None):
+    ax = pc.get_axis(subplot_arg)
+    for i in range(len(data)):
+
+        # this causes issues, quick fix:
+        label = labels[i]
+        if label.startswith('_'):
+            label = label[1:]
+
+        if isinstance(time, list):
+            t = time[i]
+        else:
+            t = time
+
+        ax.plot(t, data[i], label=label, color=DEFAULT_COLORS[i])
+        ax.plot(t, data_ref[i], label=F"{label} ref", color=DEFAULT_COLORS[i], linestyle="--")
+
+    ax.legend()
+    ax.set_title(title)
+    if not xlabel:
+        xlabel = "Time [s]"
+    ax.set_xlabel(xlabel)
+
+    if ylabel is not None:
+        ax.set_ylabel(ylabel)
+
+    if use_log:
+        ax.set_yscale('log')
+
+
+
 def bubble_plot(pc: PlotContext, xy_data, labels, y_resolution=0.1, x_resolution=0.1, title=None, ylabel=None,
                 xlabel=None, use_log=False):
     ax = pc.get_axis()
@@ -409,17 +441,29 @@ def plot_moving_boxplot_in_time_from_stats(pc: PlotContext, t, stats, title=None
     ax.fill_between(t, stats['q75'], stats['max'], alpha=0.1, lw=0, facecolor=color)
 
 
-def plot_evo_trajectory_with_euler_angles(plot_context, trajectory, label):
+def plot_evo_trajectory_with_euler_angles(plot_context, trajectory, label, ref_trajectory=None):
     traj_by_label = {
-        str(os.path.basename(label)): trajectory
+        str(label): trajectory
     }
-    plot.trajectories(plot_context.figure, traj_by_label, plot.PlotMode.xyz, subplot_arg=211)
-    time_series_plot(plot_context, trajectory.timestamps, trajectory.positions_xyz.T,
-                     ["x", "y", "z"], subplot_arg=223)
+
     rotations = R.from_quat(trajectory.orientations_quat_wxyz[:, [1, 2, 3, 0]])
     euler_angles = np.rad2deg(rotations.as_euler("ZYX")).T
     # euler_angles[2, :] = np.fmod(euler_angles[2, :] + 360, 360)
-    time_series_plot(plot_context, trajectory.timestamps, euler_angles,
-                     ["euler_z", "euler_y", "euler_x"], subplot_arg=224)
     # plot.trajectories(self._pc.figure, traj_by_label, plot.PlotMode.xy, subplot_arg=121)
     # plot.trajectories(self._pc.figure, traj_by_label, plot.PlotMode.xz, subplot_arg=122)
+
+    if ref_trajectory is not None:
+        rotations_ref = R.from_quat(ref_trajectory.orientations_quat_wxyz[:, [1, 2, 3, 0]])
+        euler_angles_ref = np.rad2deg(rotations_ref.as_euler("ZYX")).T
+        time_series_plot_with_ref(plot_context, trajectory.timestamps, trajectory.positions_xyz.T,
+                                  ref_trajectory.positions_xyz.T, ["x", "y", "z"], subplot_arg=223)
+        time_series_plot_with_ref(plot_context, trajectory.timestamps, euler_angles, euler_angles_ref,
+                                  ["euler_z", "euler_y", "euler_x"], subplot_arg=224)
+        traj_by_label[F"{label} ref"] = ref_trajectory
+    else:
+        time_series_plot(plot_context, trajectory.timestamps, trajectory.positions_xyz.T,
+                         ["x", "y", "z"], subplot_arg=223)
+        time_series_plot(plot_context, trajectory.timestamps, euler_angles,
+                         ["euler_z", "euler_y", "euler_x"], subplot_arg=224)
+
+    plot.trajectories(plot_context.figure, traj_by_label, plot.PlotMode.xyz, subplot_arg=211)